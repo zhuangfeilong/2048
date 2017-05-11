9de36ed64cb39be4bf9ad26ec52f41be4b82fa99
feilongfsx <feilongfsx@sina.com>
2017-05-11 20:15:28 +0800

    Update main2048.js

--- js/main2048.js
+++ js/main2048.js
@@ -2,28 +2,30 @@
  * Created by liuyubobobo on 14-4-11.
  * my site: http://www.liuyubobobo.com
  */
+
 var board = new Array();
 var score = 0;
+
 $(document).ready(function () {
   newgame();
 });
 
 function newgame() {
-  // 初始化游戏布局
+  //初始化棋盘格
   init();
-  // 随机生成新的数字
-  generatorNewNumber();
-  generatorNewNumber();
-
+  //在随机两个格子生成数字
+  generateOneNumber();
+  generateOneNumber();
 }
+
 function init() {
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 4; i++)
     for (var j = 0; j < 4; j++) {
-      var gridCell = $("#grid-cell-" + i + "-" + j);
+      var gridCell = $('#grid-cell-' + i + "-" + j);
       gridCell.css('top', getPostTop(i, j));
       gridCell.css('left', getPostLeft(i, j));
     }
-  }
+
   for (var i = 0; i < 4; i++) {
     board[i] = new Array();
     for (var j = 0; j < 4; j++) {
@@ -59,54 +61,30 @@ function updateBoardView() {
       }
     }
 }
-// function updateBoardView() {
-//   $(".number-cell").remove();
-//   for (var i = 0; i < 4; i++) {
-//     for (var j = 0; j < 4; j++) {
-//       $("#container").append('<div class="number-cell" id="number-cell-' + i + '-' + j + '"></div>');
-//       //numberCellBlock += numberCellBlock;
-//       //$("#container").append(numberCellBlock)
-//       var theNumberCell = $('#number-cell-' + i + '-' + j);
-//       if (board[i][j] == 0) {
-//         theNumberCell.css('width', '0px');
-//         theNumberCell.css('height', '0px');
-//         theNumberCell.css('top', getPostTop(i, j) + 50);
-//         theNumberCell.css('left', getPostLeft(i, j) + 50);
-//       } else {
-//         theNumberCell.css('width', '100px');
-//         theNumberCell.css('height', '100px');
-//         theNumberCell.css('top', getPostTop(i, j));
-//         theNumberCell.css('left', getPostLeft(i, j));
-//         theNumberCell.css('background-color', getNumberBackgroundColor(board[i][j]));
-//         theNumberCell.css('color', getNumberColor(board[i][j]));
-//         theNumberCell.text(board[i][j]);
-//       }
-//     }
-//   }
-//   // $("#container").append(numberCellBlock);
-// }
 
-function generatorNewNumber() {
-  if (nospace(board)) {
+function generateOneNumber() {
+
+  if (nospace(board))
     return false;
-  }
-  // 随机出一个数字的位置定位
-  var ranX = parseInt(Math.floor(Math.random() * 4));
-  var ranY = parseInt(Math.floor(Math.random() * 4));
+
+  //随机一个位置
+  var randx = parseInt(Math.floor(Math.random() * 4));
+  var randy = parseInt(Math.floor(Math.random() * 4));
+
   while (true) {
-    if (board[ranX][ranY] == 0)
+    if (board[randx][randy] == 0)
       break;
 
-    ranX = parseInt(Math.floor(Math.random() * 4));
-    ranY = parseInt(Math.floor(Math.random() * 4));
+    randx = parseInt(Math.floor(Math.random() * 4));
+    randy = parseInt(Math.floor(Math.random() * 4));
   }
 
-  // 随机出一个数字
+  //随机一个数字
   var randNumber = Math.random() < 0.5 ? 2 : 4;
 
-  // 随机位置显示数字
-  board[ranX][ranY] == randNumber;
-  showNumberWithAnimation(ranX, ranY, randNumber);
+  //在随机位置显示随机数字
+  board[randx][randy] = randNumber;
+  showNumberWithAnimation(randx, randy, randNumber);
 
   return true;
 }
@@ -115,25 +93,25 @@ $(document).keydown(function (event) {
   switch (event.keyCode) {
     case 37: //left
       if (moveLeft()) {
-        generatorNewNumber();
+        generateOneNumber();
         isgameover();
       }
       break;
     case 38: //up
       if (moveUp()) {
-        generatorNewNumber();
+        generateOneNumber();
         isgameover();
       }
       break;
     case 39: //right
       if (moveRight()) {
-        generatorNewNumber();
+        generateOneNumber();
         isgameover();
       }
       break;
     case 40: //down
       if (moveDown()) {
-        generatorNewNumber();
+        generateOneNumber();
         isgameover();
       }
       break;
@@ -142,19 +120,20 @@ $(document).keydown(function (event) {
   }
 });
 
-// 游戏结束的函数
 function isgameover() {
 
 }
-// 数字向左移动或合并时的变化
+
 function moveLeft() {
 
   if (!canMoveLeft(board))
     return false;
+
   //moveLeft
   for (var i = 0; i < 4; i++)
     for (var j = 1; j < 4; j++) {
       if (board[i][j] != 0) {
+
         for (var k = 0; k < j; k++) {
           if (board[i][k] == 0 && noBlockHorizontal(i, k, j, board)) {
             //move
@@ -175,13 +154,15 @@ function moveLeft() {
         }
       }
     }
+
   setTimeout("updateBoardView()", 200);
   return true;
 }
-// 数字向右移动或合并时的变化
+
 function moveRight() {
   if (!canMoveRight(board))
     return false;
+
   //moveRight
   for (var i = 0; i < 4; i++)
     for (var j = 2; j >= 0; j--) {
@@ -204,18 +185,22 @@ function moveRight() {
         }
       }
     }
+
   setTimeout("updateBoardView()", 200);
   return true;
 }
-// 数字向上移动或合并时的变化
+
 function moveUp() {
+
   if (!canMoveUp(board))
     return false;
+
   //moveUp
   for (var j = 0; j < 4; j++)
     for (var i = 1; i < 4; i++) {
       if (board[i][j] != 0) {
         for (var k = 0; k < i; k++) {
+
           if (board[k][j] == 0 && noBlockVertical(j, k, i, board)) {
             showMoveAnimation(i, j, k, j);
             board[k][j] = board[i][j];
@@ -232,18 +217,21 @@ function moveUp() {
         }
       }
     }
+
   setTimeout("updateBoardView()", 200);
   return true;
 }
-// 数字向下移动或合并时的变化
+
 function moveDown() {
   if (!canMoveDown(board))
     return false;
+
   //moveDown
   for (var j = 0; j < 4; j++)
     for (var i = 2; i >= 0; i--) {
       if (board[i][j] != 0) {
         for (var k = 3; k > i; k--) {
+
           if (board[k][j] == 0 && noBlockVertical(j, i, k, board)) {
             showMoveAnimation(i, j, k, j);
             board[k][j] = board[i][j];
@@ -260,7 +248,7 @@ function moveDown() {
         }
       }
     }
+
   setTimeout("updateBoardView()", 200);
   return true;
 }
-
